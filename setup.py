3080d4eccf962b530f8eae4789039fcb5350d573
Eric Porter <eric.porter1234@gmail.com>
2019-06-24 23:17:42 -0400

    added find packages

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 from shapes import VERSION
 
-arguments = dict(
-        name = "Shapes",
-        version = VERSION,
-        description = "2D Game Utilities",
+setup(
+    name="Shapes",
+    version=VERSION,
+    description="2D Game Utilities",
 
-        author = "Kale Kundert",
-        author_email = "kale@thekunderts.net",
+    author="Kale Kundert",
+    author_email="kale@thekunderts.net",
 
-        py_modules = ("vector", "shapes", "collisions") )
-
-setup(**arguments)
+    packages=find_packages()
+)
