ee414014a1669e6df079926582791ab2cb87250d
Eric Porter <eric.porter1234@gmail.com>
2019-06-24 23:09:24 -0400

    updated library import order

--- shapes/__init__.py
+++ shapes/__init__.py
@@ -1,5 +1,5 @@
-from .collisions import Collisions
-from .shapes import Line, Circle, Shape, Polygon, Hexagon, Rectangle
 from .vector import Vector, NullVectorError
+from .shapes import Line, Circle, Shape, Polygon, Hexagon, Rectangle
+from .collisions import Collisions
 
 VERSION = '1.0.1.0'
