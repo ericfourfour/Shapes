09af549293a5bfc6ee42861f8853f97fdb1d1821
Eric Porter <eric.porter1234@gmail.com>
2019-06-24 23:08:06 -0400

    updated version

--- shapes/__init__.py
+++ shapes/__init__.py
@@ -2,4 +2,4 @@ from .collisions import Collisions
 from .shapes import Line, Circle, Shape, Polygon, Hexagon, Rectangle
 from .vector import Vector, NullVectorError
 
-VERSION = '0.2.8'
+VERSION = '1.0.1.0'
